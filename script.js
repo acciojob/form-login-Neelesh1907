89b78fffd564a9d093054a020c7e2615550dade5
Neelesh1907 <119998532+Neelesh1907@users.noreply.github.com>
2023-09-21 13:11:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+    const fname = document.getElementsByTagName("form")[0].fname.value;
+	const lname = document.getElementsByTagName("form")[0].lname.value;
+	alert(`${fname} ${lname}`);
 }
